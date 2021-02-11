285e3b682c396bf4408ba9bf0c1daa3437b3d99c
jim winstead <jimw@trainedmonkey.com>
2021-02-11 09:26:03 -0800

    Add php-test and start of test suite

---
+++ tests/PackerTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+use Cloudstek\PhpLaff\Packer;
+
+final class PackerTest extends TestCase
+{
+    public $test_boxes= [
+      [ 'length' => 3, 'width' => '3' , 'height' => 3 ],
+      [ 'length' => 3, 'width' => '3' , 'height' => 3 ],
+      [ 'length' => 3, 'width' => '3' , 'height' => 3 ],
+      [ 'length' => 3, 'width' => '3' , 'height' => 3 ],
+      [ 'length' => 6, 'width' => '6' , 'height' => 1 ],
+      [ 'length' => 6, 'width' => '6' , 'height' => 1 ],
+      [ 'length' => 6, 'width' => '6' , 'height' => 1 ],
+    ];
+
+    public $perfect_container= [
+      'length' => 6, 'width' => 6, 'height' => 6
+    ];
+
+    public function testCreatePackerWithNoArguments(): void
+    {
+        $this->assertInstanceOf(
+            Packer::class,
+            new Packer()
+        );
+    }
+
+    public function testCreatePackerWithBoxes(): void
+    {
+        $this->assertInstanceOf(
+            Packer::class,
+            new Packer($this->test_boxes)
+        );
+    }
+
+    public function testCreatePackerWithBoxesAndContainer(): void
+    {
+        $this->assertInstanceOf(
+            Packer::class,
+            new Packer($this->test_boxes, $this->perfect_container)
+        );
+    }
+}
